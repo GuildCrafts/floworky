ca42fb4171fa50b74dcd5673c5fcd3f28cb99208
Monica Williams <monicaestellaw@gmail.com>
2016-10-06 10:53:44 -0700

    Add Item [Fixes #7] (#45)
    * created `Item` model
    * added add item form to user's index page
    * added add item route (POST /items)

--- routes/items.js
+++ routes/items.js
@@ -3,8 +3,16 @@ const router = express.Router()
 
 router.get( '/', ( request, response ) => {
   const { user } = request
-  
+
   response.render( 'items/index', { user })
+})
+
+router.post( '/', ( request, response ) => {
+  const Item = request.app.get( 'models' ).Item
+  const { title, description } = request.body
+  const user_id = request.user.id
+  Item.create({ title, description, user_id})
+    .then( result => response.redirect( '/items' ))
 })
 
 module.exports = router
