d44146124fc9221405fec09527873eb1621ade6f
jrob8577 <roberts.john@gmail.com>
2016-10-06 18:07:34 -0700

    Nested display of items [Fixes #38]
    * Introduced an ItemTree and Node for building nested Item structure
    * Added recursive output of tree to user's index page

--- routes/items.js
+++ routes/items.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+const ItemTree = require( '../src/item_tree' )
+
 router.get( '/', ( request, response ) => {
+  const Item = request.app.get( 'models' ).Item
   const { user } = request
 
-  response.render( 'items/index', { user })
+  const user_id = user.id
+  const attributes = [ 'title', 'description', 'completed', 'parent_id', 'id' ]
+
+  Item.findAll({ order: [['parent_id', 'ASC']], where: { user_id }, attributes })
+    .then( items => {
+      const tree = new ItemTree( items )
+
+      response.render( 'items/index', { user, items, tree: tree.root.children })
+    })
 })
 
 router.post( '/', ( request, response ) => {
   const Item = request.app.get( 'models' ).Item
   const { title, description } = request.body
   const user_id = request.user.id
+
   Item.create({ title, description, user_id})
     .then( result => response.redirect( '/items' ))
 })
