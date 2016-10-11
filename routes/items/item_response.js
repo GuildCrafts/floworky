f7e48dc90f9abecaae6441116ce7471ad87102ad
anasauce <anasauceda1843@gmail.com>
2016-10-11 09:50:56 -0700

    - Filter Item  [Fixes # 8]
    * Added filter option radio buttons to navbar.
    
    * Refactored Search to include complete filter (GET /items)
    
    * Updated Sequelize query to return items based on completed status

--- routes/items/item_response.js
+++ routes/items/item_response.js
@@ -1,22 +1,43 @@
-const searchQuery = require( '../../src/search_query' )
 const { pruneTree } = require( './tree_creation' )
 
 const FETCH_ATTRIBUTES = [ 'title', 'description', 'completed', 'parent_id', 'id' ]
 
-const fetchItems = user_id => (
+const allItemsQuery = user_id => (
   { order: [['createdAt', 'ASC']], where: { user_id }, FETCH_ATTRIBUTES }
 )
 
-const testForSearch = (Item, searchOptions) => ({ items, tree }) => {
-  if( searchOptions === undefined ) {
+const filteredItemsQuery = (Item, query, user_id) => ({ items, tree }) => {
+  const where = Object.assign( {}, whereSearch( query ), whereCompleted( query), { user_id } )
+
+  if( Object.keys( where ).length === 1 ) {
     return { items, tree }
   } else {
-    return Item.findAll( searchQuery( searchOptions ))
+    return Item.findAll({ where, attributes: [ 'id' ] })
       .then( pruneTree( items, tree ))
   }
+}
+
+const whereSearch = query => {
+  if( query.search ) {
+    const matchClause = { ilike: `%${query.search}%` }
+
+    return {
+      $or: [{ title: matchClause }, { description: matchClause }]
+    }
+  }
+
+  return {}
+}
+
+const whereCompleted = query => {
+  if( query.completed_filter && query.completed_filter !== 'all' ) {
+    return { completed: query.completed_filter === 'completed'}
+  }
+
+  return {}
 }
 
 const respondWithItems = (response, user) => ({ items, tree }) =>
   response.render( 'items/index', { user, items, tree: tree.children() })
 
-module.exports = { fetchItems, testForSearch, respondWithItems }
+module.exports = { allItemsQuery, filteredItemsQuery, respondWithItems }
