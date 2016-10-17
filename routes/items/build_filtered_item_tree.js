1f4c5644f80e19e341c1514935ca6d1f8b67f064
Rachel Ralston <rachelralston@gmail.com>
2016-10-17 11:27:33 -0700

    Implemented page views [Fixes #16]
    * User can view subitem and all of its children on a separate page
    * Breadcrumb navigation shows parent nodes, and links to previous pages
    * New home item created upon user sign up
    * Added migration for adding root items where they did not exist
    
    Fixed some bugs

--- routes/items/build_filtered_item_tree.js
+++ routes/items/build_filtered_item_tree.js
@@ -1,9 +1,9 @@
 const { allItemsQuery, filteredItemsQuery, respondWithItems } = require( './item_response' )
 const { buildTree } = require( './tree_creation' )
 
-const findAllItems = ( Item, user, query ) =>
+const buildFilteredItemTree = ( Item, user, query ) =>
   Item.findAll( allItemsQuery( user.id ))
     .then( buildTree )
     .then( filteredItemsQuery( Item, query, user.id ) )
 
-module.exports = findAllItems
+module.exports = buildFilteredItemTree
