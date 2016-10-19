00811413edb1b7ba32ef67bbae1138f6faf3222b
James D Stewart Jr <jamstewjr@gmail.com>
2016-10-19 14:46:34 -0700

    how to content is able to be displayed.

--- public/javascripts/help.js
+++ public/javascripts/help.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
   $( 'a.topic-selector' ).click( function( event ) {
     event.preventDefault()
 
-    $( '.topic-content iframe' ).attr( 'src', $(this).data( 'src' ) )
+    $( '.topic-content iframe' ).attr( 'src', $(this).data( 'url' ) )
     $( '.topic-content .content' ).text( $(this).data( 'content' ) )
   })
 })
