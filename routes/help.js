a3248005eb915b48e64335740cca07f54eaa2de6
James D Stewart Jr <jamstewjr@gmail.com>
2016-10-21 12:24:53 -0700

    WIP: end of week.

--- routes/help.js
+++ routes/help.js
@@ -2,13 +2,18 @@ const express = require( 'express' )
 const router = express.Router()
 
 router.get('/', ( request, response ) => {
-
   response.redirect( 'help/howtos' )
 })
 
 router.get('/howtos', ( request, response ) => {
+  const User = request.app.get('models').User
+  console.log('users are here ------------------', User)
+  const Topic = request.app.get('models').Topic
+  const UserTopic = request.app.get('models').UserTopic
 
-  response.render( 'help/howtos', { topics: data } )
+  const {users} = request.body
+
+  response.render( 'help/howtos', { topics: users } )
 })
 
 
