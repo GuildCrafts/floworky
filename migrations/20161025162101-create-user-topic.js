d75fd1311a81eb22d66eb4f0dd8c23a1bc3cfa9c
Rachel Ralston <rachelralston@gmail.com>
2016-10-25 11:01:00 -0700

    WIP getting topics data from back end

---
+++ migrations/20161025162101-create-user-topic.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('UserTopics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      topic_id: {
+        type: Sequelize.INTEGER
+      },
+      viewed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('UserTopics');
+  }
+};
