c54555338ffdc720b916848a379b531bcbe2ef56
Rachel Ralston <rachelralston@gmail.com>
2016-10-27 09:56:13 -0700

    got video topic buttons to not resort when clicked

--- routes/help/help_response.js
+++ routes/help/help_response.js
@@ -1,7 +1,7 @@
 const FETCH_ATTRIBUTES = [ 'user_id', 'topic_id', 'viewed', 'id' ]
 
 const allHelpItemsQuery = user_id => (
-  { order: [['topic_id', 'ASC']], where: { user_id }, FETCH_ATTRIBUTES }
+  { order: [[UserTopic, User, 'topic_id', 'ASC']], where: { user_id }, FETCH_ATTRIBUTES }
 )
 
 
