37291c48a370eb3af0d15b08d7060c495d22561c
Devon Wesley <dwesley89@gmail.com>
2016-10-27 16:46:03 -0700

    Server side email validation is complete. Added legit to dependencies to handle email validation. Updated api account files and created a new file in src/ mail called validate_email.

--- routes/api/v1/accounts.js
+++ routes/api/v1/accounts.js
@@ -4,12 +4,14 @@ const router = express.Router()
 const findUser = require( '../../../auth/comparePassword' )
 const authenticate = require( '../../accounts/authenticate' )
 const register = require( '../../accounts/register' )
+const validateEmail = require( '../../../src/mail/validate_email' )
 
 router.post( '/register', ( request, response ) => {
   const { User } = request.app.get( 'models' )
-  const { email, password } = request.body
+  const { email, password } =request.body
 
-  register( User, email, password )
+  validateEmail( email )
+    .then( validEmail => register( User, validEmail, password ) )
     .then( user => response.status( 201 ).json({}) )
     .catch( error => response.status( 500 ).json({}) )
 })
