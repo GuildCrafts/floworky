ccc951c42657caa4b1d32bd5f11dcf992c804bfd
Melody <mel.mustaine@gmail.com>
2016-11-03 14:45:26 -0700

    create pages that can display breadcrumbs (#95)
    Not sure which features this closes; it looks like we just added page views, without the ability to store starred pages...  At any rate, it's necessary functionality for individual page views, so merging.

--- routes/items.js
+++ routes/items.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-const { allItemsQuery, filteredItemsQuery, respondWithItems } = require( './items/item_response' )
-const { buildTree } = require( './items/tree_creation' )
+const { selectedItemsQuery, generateBreadcrumbs, respondWithItems } = require( './items/item_response' )
 const findAllItems = require('./items/find_all_items')
 
 router.get( '/', ( request, response ) => {
   const { Item } = request.app.get( 'models' )
-
   const { user, query } = request
 
   findAllItems( Item, user, query )
+    .then( generateBreadcrumbs )
+    .then( respondWithItems( user, data => response.render( 'items/index', data ) ) )
+})
+
+router.get( '/:item_id', ( request, response ) => {
+  const Item = request.app.get( 'models' ).Item
+  const { user } = request
+  const itemId = parseInt( request.params.item_id )
+  const query = selectedItemsQuery(itemId)
+
+  findAllItems( Item, user, query )
+    .then( generateBreadcrumbs )
     .then( respondWithItems( user, data => response.render( 'items/index', data ) ) )
 })
 
--- routes/items/tree_creation.js
+++ routes/items/tree_creation.js
@@ -2,10 +2,12 @@ const ItemTree = require( '../../src/item_tree' )
 
 const buildTree = items => ({ items, tree: new ItemTree( items )})
 
+const buildSubTree = item_id => ({items, tree}) =>
+  ({items, tree: tree.subTree( item_id ) })
+
 const pruneTree = (items, tree) => (ids) => {
   tree.pruneToItems( ids.map( item => item.id ))
-
   return { items, tree }
 }
 
-module.exports = { buildTree, pruneTree }
+module.exports = { buildTree, buildSubTree, pruneTree }
