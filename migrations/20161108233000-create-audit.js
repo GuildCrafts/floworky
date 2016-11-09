32ce45fc3164413823c96684a39b7b4fd04b8169
anasauce <anasauceda1843@gmail.com>
2016-11-08 17:13:41 -0800

    added migration for audit table

---
+++ migrations/20161108233000-create-audit.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Audits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      item_id: {
+        type: Sequelize.INTEGER
+      },
+      field_name: {
+        type: Sequelize.STRING
+      },
+      old_value: {
+        type: Sequelize.STRING
+      },
+      new_value: {
+        type: Sequelize.STRING
+      },
+      field_type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Audits');
+  }
+};
