3a2571ea4744bb8d0b93578f468db368ac47f71c
Shakalee14 <shakalee14@gmail.com>
2016-11-09 15:30:55 -0800

    html and plain text switching

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,40 +1,37 @@
-$(document).ready(function(){
+$(document).ready( () => {
   const modal = document.querySelector('.modal')
   const exportLink = document.querySelector('.dropdown__item--export-link')
-  const buttonFormattedText = document.getElementById('formatted-preview')
+  const buttonFormattedText = document.getElementById('formatted-text-preview')
   const buttonPlainText = document.getElementById('plain-text-preview')
   const downloadLink = document.querySelector('.download-link')
-  const elHtml = document.querySelector('.item-page')
-  const previewText = document.querySelector('preview-text')
-
-  buttonPlainText.click(function(){
-    var now = new Date().toString();
-    this.href = "data:text/plain;charset=UTF-8,"  + encodeURIComponent(now);
-});
+  const redlink = document.querySelector('.change-text-to-red')
+  const previewText = document.querySelector('#preview-text')
+  const closeLink = document.querySelector('.preview-window__close-link')
 
   exportLink.onclick = e => {
     modal.style.display = 'block'
-    // const mimeType = 'text/plain'
-    const exportLink = document.querySelector('.dropdown__item--export-link')
+    $("#preview-text").replaceWith($( "#preview-text.item-page.html__tree" ).text())
     e.preventDefault()
   }
 
+  buttonPlainText.onclick = e => {
 
+    $( '#preview-text-text.item-page.text__tree.hidden' )
+      .toggle( 'hidden' )
 
-  const closeLink = document.querySelector('.preview-window__close-link')
+    e.preventDefault()
+  }
+
+  buttonFormattedText.onclick = e => {
+
+    $( '#preview-text.item-page.html__tree.show' ).toggle('show')
+
+    e.preventDefault()
+  }
 
   closeLink.onclick = e => {
     modal.style.display = 'none'
     e.preventDefault()
   }
-
-  // downloadLink.onclick = (e) => {
-  //   downloadInnerHTML = (filename, elId, mimeType) => {
-  //     mimeType = mimeType || 'text/plain'
-
-  //     link.setAttribute('download', filename)
-  //     link.setAttribute('href', 'data:' +  mimeType + ';charset=utf-8' + encodeURIComponent(elHtml))
-  //   }
-  // }
 
 })
