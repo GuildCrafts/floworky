0e683071f78c8b403235ae46d2a620cb87c1c62a
Shakalee14 <shakalee14@gmail.com>
2016-11-10 12:00:24 -0800

    handled jrobs comments and some of our own ... currently working on downloading info instead of the whole page

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,30 +1,37 @@
 $(document).ready( () => {
-  const modal = document.querySelector('.modal')
-  const openModal = document.querySelector('.dropdown__item--export-link')
-  const buttonFormattedText = document.getElementById('formatted-text-preview')
-  const buttonPlainText = document.getElementById('plain-text-preview')
-  const closeModal = document.querySelector('.preview-window__close-link')
+  const modal = document.querySelector( '.modal' )
+  const openModal = document.querySelector( '.dropdown__item--export-link' )
+  const closeModal = document.querySelector( '.preview-window--footer__close-link' )
+  const buttonPlainText = document.getElementById( 'plain-text-preview' )
+  const buttonFormattedText = document.getElementById( 'formatted-text-preview' )
+  const downloadLink = document.getElementById( 'downloadlink' )
 
   openModal.onclick = e => {
-    modal.style.display = 'block'
     e.preventDefault()
+    modal.style.display = 'block'
   }
 
   closeModal.onclick = e => {
-    modal.style.display = 'none'
     e.preventDefault()
+    modal.style.display = 'none'
   }
 
   buttonPlainText.onclick = e => {
-    $( '#preview-text-text.item-page.text__tree.hidden' )
-      .toggle( 'hidden' ).focus()
     e.preventDefault()
+
+    $( '#preview-text-text.item-page.text__tree.hidden' ).removeClass( 'hidden' ).addClass( 'show' )
+    $( '#preview-text.item-page.html__tree.show' ).removeClass( 'show' ).addClass( 'hidden' )
   }
 
   buttonFormattedText.onclick = e => {
-    $( '#preview-text.item-page.html__tree.show' ).toggle('show').focus()
     e.preventDefault()
+    $( '#preview-text.item-page.html__tree.hidden' ).removeClass( 'hidden' ).addClass( 'show' )
+    $( '#preview-text-text.item-page.text__tree.show' ).removeClass( 'show' ).addClass( 'hidden' )
   }
 
+  downloadLink.click = e => {
+    console.log('innertext', $('#preview-text-text.item-page.text__tree.hidden').html() )
 
+    downloadInnerHTML(filename, 'text/html')
+  }
 })
