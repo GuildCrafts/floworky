86404e64998b38193c3ea1eab9d3486acd07de14
Shakalee14 <shakalee14@gmail.com>
2016-11-09 16:50:06 -0800

    first incremental pr

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,37 +1,30 @@
 $(document).ready( () => {
   const modal = document.querySelector('.modal')
-  const exportLink = document.querySelector('.dropdown__item--export-link')
+  const openModal = document.querySelector('.dropdown__item--export-link')
   const buttonFormattedText = document.getElementById('formatted-text-preview')
   const buttonPlainText = document.getElementById('plain-text-preview')
-  const downloadLink = document.querySelector('.download-link')
-  const redlink = document.querySelector('.change-text-to-red')
-  const previewText = document.querySelector('#preview-text')
-  const closeLink = document.querySelector('.preview-window__close-link')
+  const closeModal = document.querySelector('.preview-window__close-link')
 
-  exportLink.onclick = e => {
+  openModal.onclick = e => {
     modal.style.display = 'block'
-    $("#preview-text").replaceWith($( "#preview-text.item-page.html__tree" ).text())
+    e.preventDefault()
+  }
+
+  closeModal.onclick = e => {
+    modal.style.display = 'none'
     e.preventDefault()
   }
 
   buttonPlainText.onclick = e => {
-
     $( '#preview-text-text.item-page.text__tree.hidden' )
-      .toggle( 'hidden' )
-
+      .toggle( 'hidden' ).focus()
     e.preventDefault()
   }
 
   buttonFormattedText.onclick = e => {
-
-    $( '#preview-text.item-page.html__tree.show' ).toggle('show')
-
+    $( '#preview-text.item-page.html__tree.show' ).toggle('show').focus()
     e.preventDefault()
   }
 
-  closeLink.onclick = e => {
-    modal.style.display = 'none'
-    e.preventDefault()
-  }
 
 })
