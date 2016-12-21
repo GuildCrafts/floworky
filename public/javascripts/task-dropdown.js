bbb63d4fba880bba85df0548b7c3dbf9de297c42
TJ Walker <tjfwalker+git@gmail.com>
2016-12-21 13:41:39 -0800

    Pacify the great Jrob
    Abide by the canonical code style.

--- public/javascripts/task-dropdown.js
+++ public/javascripts/task-dropdown.js
@@ -1,11 +1,11 @@
-const taskMenuTriggers = document.querySelectorAll('.item__toggle')
+const taskMenuTriggers = document.querySelectorAll( '.item__toggle' )
 
 taskMenuTriggers.forEach( trigger => {
-  trigger.addEventListener('mouseenter', event => {
-    event.target.children[0].style.display = 'block'
+  trigger.addEventListener( 'mouseenter', event => {
+    event.target.children[ 0 ].style.display = 'block'
   })
-  trigger.addEventListener('mouseleave', event => {
-    event.target.children[0].style.display = 'none'
+  trigger.addEventListener( 'mouseleave', event => {
+    event.target.children[ 0 ].style.display = 'none'
   })
 })
 
