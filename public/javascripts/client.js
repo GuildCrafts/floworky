492997418306cb38756df51801f4fda29e5eead3
TJ Walker <tjfwalker+git@gmail.com>
2016-12-21 13:54:04 -0800

    Refactor to Align with Official Code Style
    Placate Jrob

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,36 +1,29 @@
-$(document).ready( () => {
+$( document ).ready( () => {
   const modal = document.querySelector( '.modal' )
-  const openModal = document.querySelector( '.export-link' )
-  const closeModal = document.querySelector( '.preview-window--footer__close-link' )
-  const buttonPlainText = document.getElementById( 'plain-text-preview' )
-  const buttonFormattedText = document.getElementById( 'formatted-text-preview' )
-  const downloadLink = document.getElementById( 'downloadlink' )
-  const innerText = $('#preview-text-text').html()
 
-  openModal.onclick = e => {
-    e.preventDefault()
+  document.querySelector( '.export-link' ).onclick = event => {
+    event.preventDefault()
 
     modal.style.display = 'block'
   }
 
-  closeModal.onclick = e => {
-    e.preventDefault()
+  document.querySelector( '.preview-window--footer__close-link' ).onclick = event => {
+    event.preventDefault()
 
     modal.style.display = 'none'
   }
 
-  buttonPlainText.onclick = e => {
+  document.getElementById( 'plain-text-preview' ).onclick = event => {
     $( '#preview-text-text.item-page.text__tree.hidden' ).removeClass( 'hidden' ).addClass( 'show' )
     $( '#preview-text.item-page.html__tree.show' ).removeClass( 'show' ).addClass( 'hidden' )
     $( '.plain-text-download' ).removeClass( 'hidden' ).addClass( 'show' )
     $( '.html-text-download' ).removeClass( 'show' ).addClass( 'hidden' )
   }
 
-  buttonFormattedText.onclick = e => {
+  document.getElementById( 'formatted-text-preview' ).onclick = event => {
     $( '#preview-text.item-page.html__tree.hidden' ).removeClass( 'hidden' ).addClass( 'show' )
     $( '#preview-text-text.item-page.text__tree.show' ).removeClass( 'show' ).addClass( 'hidden' )
     $( '.html-text-download' ).removeClass( 'hidden' ).addClass( 'show' )
     $( '.plain-text-download' ).removeClass( 'show' ).addClass( 'hidden' )
   }
-
 })
