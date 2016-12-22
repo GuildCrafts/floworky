ef72f0005e4bc73bfd0fccf30b774d9bd5ddf9d7
TJ Walker <tjfwalker+git@gmail.com>
2016-12-22 08:58:44 -0800

    Added whitespace in task dropdown event listeners

--- public/javascripts/task-dropdown.js
+++ public/javascripts/task-dropdown.js
@@ -4,6 +4,7 @@ taskMenuTriggers.forEach( trigger => {
   trigger.addEventListener( 'mouseenter', event => {
     event.target.children[ 0 ].style.display = 'block'
   })
+
   trigger.addEventListener( 'mouseleave', event => {
     event.target.children[ 0 ].style.display = 'none'
   })
