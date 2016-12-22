1da55bea579d271353b1e45d4d4b0ae8075bf2da
generalmeow <rcbrown@gmail.com>
2016-12-21 16:40:30 -0800

    WIP - added delete link to items/index.pug, click function to jQuery to mimic dropdown delete, and delete functionality to route to change is_delete flag to true

--- routes/items.js
+++ routes/items.js
@@ -74,5 +74,22 @@ router.post( '/:id', ( request, response ) => {
     )
 })
 
+router.post( '/delete/:id', ( request, response ) => {
+  const { Item, Audit } = request.app.get( 'models' )
+  const { id } = request.params
+  const user_id = parseInt( request.user.id )
+
+  Item.findOne({ where: { id, user_id } })
+    .then( item => {
+      const softDeletedItem = item
+      softDeletedItem.update( Item.filterParameters( { is_deleted: true } ) )
+      return softDeletedItem
+    })
+    .then( createAuditEntries( Item, Audit, user_id ) )
+    .then( result => response.json({ success: true, id }))
+    .catch( error =>
+      response.json({ success: false, id, message: error.message })
+    )
+})
 
 module.exports = router
